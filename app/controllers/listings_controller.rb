597dfb0bc135fa02c46072a7c08b8b71ab949ee5
Thais Pulliam <thaispulliam@gmail.com>
2015-12-15 15:41:32 -0500

    Fix bookings into listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -9,6 +9,10 @@ class ListingController < ApplicationController
     @listing = Listing.find(params[:id])
     @neighborhood = Neighborhood.where(:id => @listings.neighborhood_id)
     @reviews = Review.where(listing_id: params[:id])
+
+    #For the new Booking form:
+    @booking = Booking.new
+	@guest = session[:user_id]
   end
 
 end

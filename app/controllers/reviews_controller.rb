ca282b29ade5d4d09e8c54b60d89944a980c49f9
frances harlow <frances.harlow@gmail.com>
2015-12-15 15:23:31 -0500

    adds reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,7 +3,9 @@ class ReviewsController < ApplicationController
   before_action :authorize, except: [:index, :show]
 
   def index
+    @listing = Listing.find(params[:id])
     @reviews = Review.where(listing_id: params[:id])
+    # @reviews = @listing.reviews
   end
 
   def show
@@ -18,26 +20,16 @@ class ReviewsController < ApplicationController
     @review = Review.create({
       title: params[:title],
       body: params[:body],
-      guest_id: session[:user_id]
+      guest_id: session[:user_id],
+      listing_id: params[:id],
+      created_at: DateTime.now,
       })
     redirect_to reviews_path
   end
 
+  def destroy
+    @review = Review.find(params[:id]).destroy
+    redirect_to reviews_path
+  end
+
 end
-
-  # create_table "reviews", force: :cascade do |t|
-  #   t.string   "title"
-  #   t.string   "body"
-  #   t.integer  "guest_id"
-  #   t.integer  "listing_id"
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  # end
-
-        title: params["title"],
-      runtime: params["runtime"],
-      rating: params["rating"],
-      actors: params["actors"],
-      release_year: params["release_year"],
-      img_url: params["img_url"]
-      })
